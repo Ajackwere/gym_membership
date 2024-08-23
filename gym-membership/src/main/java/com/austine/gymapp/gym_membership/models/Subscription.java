5dc13df1bb0ef18cee51440fb3b295ee427fe2c6
ajackwere <austinewere59@gmail.com>
2024-08-23 15:17:10 +0300

    add subscription model

--- gym-membership/src/main/java/com/austine/gymapp/gym_membership/models/Subscription.java
+++ gym-membership/src/main/java/com/austine/gymapp/gym_membership/models/Subscription.java
@@ -1,5 +1,76 @@
 package com.austine.gymapp.gym_membership.models;
+import jakarta.persistence.*;
+import java.time.LocalDate;
 
+
+@Entity
+@Table(name = "subscriptions")
 public class Subscription {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "member_id", nullable = false)
+    private Member member;
+
+    @ManyToOne
+    @JoinColumn(name = "membership_plan_id", nullable = false)
+    private MembershipPlan membershipPlan;
+
+    @Column(nullable = false)
+    private LocalDate startDate;
+
+    @Column(nullable = false)
+    private LocalDate endDate;
+
+
+
+    @Column(nullable = false)
+    private boolean isActive;
+
+    public Long getId(){
+        return id;
+    }
+
+    public void setId(Long id){
+        this.id = id;
+    }
+
+    public LocalDate getStartDate(){
+        return startDate;
+    }
+
+    public void setStartDate(LocalDate startDate){
+        this.startDate = startDate;
+    }
+
+    public Member getMember(){
+        return member;
+    }
+
+    public void setMember(Member member){
+        this.member = member;
+    }
+
+    public MembershipPlan getMembershipPlan(){
+        return membershipPlan;
+    }
+
+    public void setMembershipPlan(MembershipPlan membershipPlan){
+        this.membershipPlan = membershipPlan;
+    }
+
+    public LocalDate getEndDate(){
+        return endDate;
+    }
+
+    public boolean isActive(){
+        return isActive;
+    }
+
+    public void setActive(boolean isActive){
+        this.isActive = isActive;
+    }
 
 }
